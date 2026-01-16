9300ebb05ffdeb19b980c535196d63d30b51d864
roman <roman@ananas>
2026-01-16 15:25:49 +0000

    change lib api

--- src/mathlib.cpp
+++ src/mathlib.cpp
@@ -1,7 +1,6 @@
-#include "mathlib.h"
+#include "mathlib.hpp"
 #include <math.h>
 #include <float.h>
-#include <limits.h>
 
 MathError math_add(double a, double b, double* result) {
     if (!result) {
@@ -14,7 +13,6 @@ MathError math_add(double a, double b, double* result) {
     }
     
     *result = a + b;
-    
     return MATH_OK;
 }
 
@@ -29,7 +27,6 @@ MathError math_sub(double a, double b, double* result) {
     }
     
     *result = a - b;
-    
     return MATH_OK;
 }
 
@@ -51,7 +48,6 @@ MathError math_mul(double a, double b, double* result) {
     }
     
     *result = a * b;
-    
     return MATH_OK;
 }
 
@@ -59,7 +55,7 @@ MathError math_div(double a, double b, double* result) {
     if (!result) {
         return MATH_ERR_INVALID_ARG;
     }
-
+    
     if (b == 0.) {
         return MATH_ERR_DIV_ZERO;
     }
@@ -72,11 +68,10 @@ MathError math_div(double a, double b, double* result) {
     }
     
     *result = a / b;
-    
     return MATH_OK;
 }
 
-MathError math_pow(double base, long long exp, double* result) {
+MathError math_pow(double base, double exp, double* result) {
     if (!result) {
         return MATH_ERR_INVALID_ARG;
     }
@@ -100,54 +95,55 @@ MathError math_pow(double base, long long exp, double* result) {
     }
     
     bool negative_exp = exp < 0;
-    unsigned long long abs_exp = negative_exp ? (unsigned long long)(-exp) : (unsigned long long)exp;
+    double abs_exp = fabs(exp);
     
     *result = 1.;
     
     while (abs_exp > 0) {
         MathError status = math_mul(*result, base, result);
-        if (status != MATH_OK)
+        if (status != MATH_OK) {
             return status;
-        --abs_exp;
+        }
+        abs_exp -= 1.;
     }
     
     if (negative_exp) {
         if (fabs(*result) < 1. / DBL_MAX) {
             return MATH_ERR_OVERFLOW;
         }
-        
         *result = 1. / (*result);
     }
     
     return MATH_OK;
 }
 
-MathError math_factorial(long long n, unsigned long long* result) {
-    if (!result || n < 0) {
+MathError math_factorial(double n, double* result) {
+    if (!result) {
         return MATH_ERR_INVALID_ARG;
     }
     
-    if (n <= 1) {
-        *result = 1;
+    if (n < 0 || floor(n) != n) {
+        return MATH_ERR_INVALID_ARG;
+    }
+    
+    // 170! ≈ 7.26e306, 171! > DBL_MAX
+    if (n > 170) {
+        return MATH_ERR_OVERFLOW;
+    }
+    
+    if (n <= 1.) {
+        *result = 1.;
         return MATH_OK;
     }
     
-    if (n > 20) {
-        return MATH_ERR_OVERFLOW;
-    }
-    
-    unsigned long long sub_result = 0;
+    double sub_result = 1.;
     MathError status = math_factorial(n - 1, &sub_result);
-    
-    if (status != MATH_OK)
+
+    if (status != MATH_OK) {
         return status;
-
-    if (n > ULLONG_MAX / sub_result) {
-        return MATH_ERR_OVERFLOW;
     }
-
-    *result = n * sub_result;
-    return MATH_OK;
+    
+    return math_mul(n, sub_result, result);
 }
 
 const char* math_error_string(MathError err) {
